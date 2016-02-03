f57bbb94bc89b2b20368d98d99ed70139d107572
hulei02 <hulei02@meituan.com>
2016-02-03 16:16:01 +0800

    change template to swig

--- app.js
+++ app.js
@@ -7,12 +7,17 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var swig  = require('swig');
+var cons = require('consolidate');
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+//app.set('views', path.join(__dirname, 'views'));
+//app.set('view engine', 'swig');
+
+app.engine('.html', cons.swig);
+app.set('view engine', 'html');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
